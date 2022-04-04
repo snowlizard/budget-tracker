e21ca02c15e8a57418e33deb02de1236bbe2e59e
julian <illegal_burrito@live.com>
2022-04-04 16:57:13 -0700

    adding ci env

--- src/js/rootStore.js
+++ src/js/rootStore.js
@@ -2,7 +2,6 @@ import { createStore } from 'redux';
 import rootReducer from './rootReducer';
 
 const rootStore = createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    rootReducer);
 
 export default rootStore;
